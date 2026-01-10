088ee1906ffca4c021483f14de66d8fd0aa493af
Sattaksh Ratan <146627554+Sattaksh@users.noreply.github.com>
2026-01-10 14:19:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const q = id => document.getElementById(id);
   const searchBox = q("searchBox"), searchBtn = q("searchBtn"), voiceBtn = q("voiceBtn");
   const clearBtn = document.getElementById("clearBtn");
-  let CURRENT_MODEL = "xiaomi/mimo-v2-flash:free";
+  let CURRENT_MODEL = "gemini-gemma-3-12b";
   let uploadedImageData = null;
 
 // Show/hide ✖ when typing
