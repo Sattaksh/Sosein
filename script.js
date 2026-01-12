8baeb18a5d2326919aa4df718cb302da1a15fbe8
Sattaksh Ratan <146627554+Sattaksh@users.noreply.github.com>
2026-01-12 13:34:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -45,7 +45,7 @@ if (heroTitle && !document.body.classList.contains("searching")) {
   const q = id => document.getElementById(id);
   const searchBox = q("searchBox"), searchBtn = q("searchBtn"), voiceBtn = q("voiceBtn");
   const clearBtn = document.getElementById("clearBtn");
-  let CURRENT_MODEL = "z-ai/glm-4.5-air:free"; //xiaomi/mimo-v2-flash:free
+  let CURRENT_MODEL = "nvidia/nemotron-3-nano-30b-a3b:free"; //xiaomi/mimo-v2-flash:free
   let uploadedImageData = null;
   
 
@@ -650,12 +650,14 @@ if (enhance) {
 async function fetchAIAnswer(question, imageData) {
   const FALLBACK_MODELS = [
     CURRENT_MODEL,// whatever user selected
+    "nvidia/nemotron-nano-12b-v2-vl:free",
+    "z-ai/glm-4.5-air:free",
+    "tngtech/deepseek-r1t2-chimera:free",
     "gemini-3-flash-preview",
     "gemini-2.5-flash",
     "gemini-2.5-flash-lite",
     "xiaomi/mimo-v2-flash:free",
-    "nvidia/nemotron-3-nano-30b-a3b:free",     // fast & reliable
-    "tngtech/deepseek-r1t2-chimera:free",
+     // fast & reliable
     // last-resort thinker
   ];
 
